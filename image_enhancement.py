af1b6a744f09ddc10d578e5a279e3ea3b757f7f9
Sarah <sarahunoke@gmail.com>
2026-01-07 13:49:07 +0100

    adjusted outscalue value

--- image_enhancement.py
+++ image_enhancement.py
@@ -29,7 +29,7 @@ class RealESRGANRunner:
             "-n", self.model_name,
             "-i", str(self.input_folder),
             "-o", str(self.output_folder),
-            "--outscale", int(self.outscale)
+            "--outscale", str(self.outscale)
         ]
         if self.face_enhance:
             cmd.append("--face_enhance")
